58e236e10e4eb1ca86f7810d8f2a6a00971f41ba
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-03-22 03:08:58 -0400

    Create opfun3.c

---
+++ opfun3.c
@@ -0,0 +1,94 @@
+#include "monty.h"
+
+/**
+ * mul - s
+ * @stack: Double linked list
+ * @line_number: File line execution
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n",
+			line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n = (*stack)->next->n * (*stack)->n;
+	pop(stack, line_number);
+}
+
+
+/**
+ * mod - s
+ * @stack: Double linked list
+ * @line_number: File line execution
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n",
+			line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n = (*stack)->next->n % (*stack)->n;
+	pop(stack, line_number);
+}
+
+/**
+ * pchar - s
+ * @stack: Double linked list
+ * @line_number: File line execution
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+	if (isascii((*stack)->n))
+	{
+		printf("%c\n", (*stack)->n);
+		return;
+	}
+	fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+	free_all();
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * pstr - s
+ * @stack: Double linked list
+ * @line_number: File line execution
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = *stack;
+	(void) line_number;
+
+	if (!stack || !*stack)
+	{
+		putchar('\n');
+		return;
+	}
+	while (tmp)
+	{
+		if (tmp->n == 0)
+			break;
+		if (!isascii((tmp)->n))
+			break;
+		putchar(tmp->n);
+		tmp = tmp->next;
+	}
+	putchar('\n');
+}
